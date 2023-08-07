c322f2ac7437b6b199a17fd26347ec47944ba2b2
notMrpod <darsh.pod@gmail.com>
2023-08-07 01:51:38 -0500

    EmailJS Changes

--- sendEmail.js
+++ sendEmail.js
@@ -1,20 +1,36 @@
-const emailjs = require('emailjs-com');
+// Load environment variables from .env file (assuming you have dotenv configured)
+require('dotenv').config();
 
-// Set up EmailJS with your user ID from the .env file
-emailjs.init(process.env.EMAILJS_USER_ID);
+// Initialize EmailJS with your public key from the .env file
+emailjs.init(process.env.EMAILJS_PUBLIC_KEY);
 
-// Email content using environment variables from the .env file
-const emailData = {
-    to: process.env.EMAIL_TO,
-    subject: process.env.EMAIL_SUBJECT,
-    text: process.env.EMAIL_TEXT,
-};
+// Function to send the email
+function sendEmail() {
+    // Gather data from form fields
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phoneNumber = document.getElementById('phoneNumber').value;
+    const websiteUrl = document.getElementById('websiteUrl').value;
+    const message = document.getElementById('message').value;
 
-// Send the email using the service and template IDs from the .env file
-emailjs.send(process.env.EMAIL_SERVICE_ID, process.env.EMAIL_TEMPLATE_ID, emailData)
-    .then((response) => {
-        console.log('Email sent:', response);
-    })
-    .catch((error) => {
-        console.error('Error sending email:', error);
-    });
+    // Email content
+    const emailData = {
+        to: process.env.EMAIL_TO,
+        subject: process.env.EMAIL_SUBJECT,
+        text: `Name: ${name}\nEmail: ${email}\nPhone Number: ${phoneNumber}\nWebsite: ${websiteUrl}\n\nMessage:\n${message}`,
+    };
+
+    // Send the email using the service and template IDs from the .env file
+    emailjs.send(process.env.EMAIL_SERVICE_ID, process.env.EMAIL_TEMPLATE_ID, emailData)
+        .then((response) => {
+            console.log('Email sent:', response);
+            // You can add additional code to handle success, display messages, etc.
+        })
+        .catch((error) => {
+            console.error('Error sending email:', error);
+            // You can add additional code to handle errors, display messages, etc.
+        });
+}
+
+// Attach the sendEmail function to the "Send Message" button
+document.getElementById('send-button').addEventListener('click', sendEmail);
